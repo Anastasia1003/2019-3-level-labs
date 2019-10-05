5576235442db12f909dcd74777e0b481c5bdfefe
Anastasia <rogulinaanastasia@gmail.com>
2019-10-05 21:23:57 +0300

    added parser.py and other files

---
+++ lab_1/parser.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import requests
+import simplejson as json
+import datetime
+import io
+from bs4 import BeautifulSoup
+
+page_url = 'https://www.livejournal.com/category/nauka-i-tehnika/'
+
+
+def get_html_page(page_url):
+    return requests.get(page_url)
+
+
+def find_articles(html_page):
+    lj_content = html_page.text
+
+    soup = BeautifulSoup(lj_content)
+    articles = list()
+    for article in soup.find_all('h2'):
+        text = article.text.strip()
+        articles.append(text)
+    return articles
+
+
+def publish_report(path, articles):
+    result = {"url": page_url,
+              "creationDate": datetime.datetime.now().strftime("%b %m, %Y, %H:%M"),
+              "articles": articles}
+
+    path = "articles.json"
+    with io.open(path, 'w', encoding='utf8') as json_file:
+        json.dump(result, json_file, ensure_ascii=False)
+
+
+html_page = get_html_page(page_url)
+articles = find_articles(html_page)
+publish_report('', articles)
