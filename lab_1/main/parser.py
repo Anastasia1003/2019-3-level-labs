3fd4e3bffdb50c538d9c4b3d0168b58ddf2c00f4
Anastasia <rogulinaanastasia@gmail.com>
2019-10-16 12:29:49 +0300

    app change

--- lab_1/main/parser.py
+++ lab_1/main/parser.py
@@ -14,7 +14,7 @@ def get_html_page(page_url):
 
 def find_articles(html_page):
 
-    soup = BeautifulSoup(html_page, 'html.parser')
+    soup = BeautifulSoup(html_page.text, 'html.parser')
     articles = []
 
     for article in soup.find_all('h2'):
@@ -34,5 +34,5 @@ def publish_report(path, articles):
 
 
 html_page = get_html_page(page_url)
-articles = find_articles(html_page.text)
+articles = find_articles(html_page)
 publish_report(path, articles)
